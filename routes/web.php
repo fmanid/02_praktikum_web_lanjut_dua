f4d9a85f4ee439816e3027ea7d89bf70d439dd83
fmanid4 <fmanid88@emailme.my.id>
2021-03-03 09:10:39 +0700

    commit2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,6 +20,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', [PageController::class,'index']);
+Route::get('/', [HomeController::class,'index']);
 Route::get('/about', [PageController::class,'about']);
-Route::get('/articles/{id}', [PageController::class,'articles']);
+Route::get('/articles/{id}', [ArticleController::class,'articles']);
